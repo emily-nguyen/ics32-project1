62ab7a97b309266d8a1209db048ff90df3ac5e67
emily-nguyen <emily-nguyen@users.noreply.github.com>
2015-01-15 14:50:01 -0800

    Created recursive function that returns master list of all the files in directory

--- ICS 32 Project 1.py
+++ ICS 32 Project 1.py
@@ -53,7 +53,7 @@ def search_parameters(path: str)->list:
     os.chdir(path)
     lst = os.listdir(path)
     start_list = path_list(lst)
-    result, names, value = [], [], ''
+    result, value, new_list = [], '', []
 
     while True:
         try: 
@@ -63,95 +63,88 @@ def search_parameters(path: str)->list:
         except:
             pass
 
-        length = len(input_list) 
+        length = len(input_list)
+        master = master_list(start_list, result)
 
         if search_type == 'N' and length >= 2: 
-            new_list = search_name(names, start_list, result)
+            lst2 = search_name(names, master, new_list)
             return new_list 
         elif search_type == 'E' and length == 2:
-            new_list = search_extension(value, start_list, result)
+            lst2 = search_extension(value, master, new_list)
             return new_list 
         elif search_type == 'S' and length == 2:
             if is_num(value): 
-                new_list = search_size(int(value), start_list, result)
-                return new_list
+                lst2 = search_size(int(value), master, new_list)
+                return lst2
             print('ERROR')
         else:
             print('ERROR')
 
 
-def add_change(item: 'str', start_list: list)->None:
+def add_change(item: 'str', result: list)->None:
     '''Changes the directory, add file paths to list of paths, and changes back to
     parent directory'''
    
     os.chdir(item)
     lst = os.listdir(item)
     for i in lst:
-        start_list.append(os.path.realpath(i))
+        result.append(os.path.realpath(i))
     os.chdir('.')
-	
 
-def search_name(name_list: list, start_list: list, result: list)->list:
-    '''Searches if files match those in the name list; return list of matching files;
-    else prints ERROR'''
-	    
+
+def master_list(start_list: list, result: list)->list:
+    '''Performs recursion to return a master list of files in a given directory'''
+
     if start_list == []:
         return result
     else:
         item = start_list[0]
-        basename = os.path.basename(item)
 
         if os.path.isfile(item):
-            if basename in name_list:
-                result.append(item)
-            return search_name(name_list, start_list[1:], result)
+            result.append(item)
+            return master_list(start_list[1:], result)
         elif os.path.isdir(item):
-            add_change(item, start_list)
-            return search_name(name_list, start_list[1:], result)
+            add_change(item, result)
+            return master_list(start_list[1:], result)
         else:
             print('ERROR')
+    
+
+def search_name(name_list: list, result: list, new_list: list)->list:
+    '''Searches if files match those in the name list; return list of matching files;
+    else prints ERROR'''
+
+    for item in result:
+        basename = os.path.basename(item)
+        
+        if basename in name_list: 
+            new_list.append(item)
+    return new_list
 
 
-def search_extension(ending: str, start_list: list, result: list)->list:
+
+def search_extension(ending: str, result: list, new_list: list)->list:
     '''Searches if files match extension specified; returns list of matching files;
     else prints ERROR'''
 
-    if start_list == []:
-        return result
-    else:
-        item = start_list[0]
+    for item in result:
         basename = os.path.basename(item)
-
-        if os.path.isfile(item):
-            if basename.endswith(ending):
-                result.append(item)
-            return search_extension(ending, start_list[1:], result)
-        elif os.path.isdir(item):
-            add_change(item, start_list)
-            return search_extension(ending, start_list[1:], result)
-        else:
-            print('ERROR')
+        
+        if basename.endswith(ending):
+            new_list.append(item)
+    return new_list
 
 
-def search_size(size: int, start_list: list, result: list)->list:
+def search_size(size: int, result: list, new_list)->list:
     '''Searches if files' sizes are greater than size specified; returns list of matching
     files; else prints ERROR'''
 
-    if start_list == []:
-        return result
-    else:
-        item = start_list[0]
+    for item in result:
         basename = os.path.basename(item)
-
-        if os.path.isfile(item):
-            if os.path.getsize(item) > size:
-                result.append(item)
-            return search_size(size, start_list[1:], result)
-        elif os.path.isdir(item):
-            add_change(item, start_list)
-            return search_size(size, start_list[1:], result)
-        else:
-            print('ERROR')
+        
+        if os.path.getsize(item) > size:
+            new_list.append(item)
+    return new_list
 
 
 def open_file(filename: str):
@@ -219,4 +212,3 @@ def main():
     
 if __name__ == '__main__':
     main()
-    
