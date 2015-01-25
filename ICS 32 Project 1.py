c7630d65fb34c949db9e055edf2ff05b0a348588
Emily Nguyen <emilynguyen5@outlook.com>
2015-01-24 20:14:11 -0800

    Update ICS 32 Project 1.py

--- ICS 32 Project 1.py
+++ ICS 32 Project 1.py
@@ -1,10 +1,5 @@
 # ICS 32 Project #1
 
-'''Program that takes in 3 lines of input: (1) directory path, (2) search characteristic,
-(3) action to perform on file. Uses recursion to look for files in subdirectories and 
-filters files by specific criteria.'''
-
-
 import os
 import pathlib
 import shutil
